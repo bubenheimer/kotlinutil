667f010dc114c0a5b02bec1033c225aac2d9acf5
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2021-03-14 09:26:54 -0500

    Add Timer flow with operators

---
+++ src/commonMain/kotlin/org/bubenheimer/util/flow/Timers.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015-2021 Uli Bubenheimer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.bubenheimer.util.flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.takeWhile
+import kotlin.time.Duration
+import kotlin.time.TimeMark
+import kotlin.time.TimeSource
+
+public fun TimeSource.fixedRateFlow(interval: Duration): Flow<Unit> = flow {
+    var nextMark = markNow()
+
+    while (true) {
+        emit(Unit)
+        delay(interval - nextMark.elapsedNow())
+        nextMark += interval
+    }
+}
+
+public fun <T> Flow<T>.takeFor(
+    interval: Duration,
+    timeSource: TimeSource = TimeSource.Monotonic
+): Flow<T> = flow {
+    val endMark: TimeMark = timeSource.markNow() + interval
+
+    emitAll(takeWhile { endMark.hasNotPassedNow() })
+}
