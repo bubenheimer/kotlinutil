a18c8e22c9ccb4126df7dc6299c718a57e940c02
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2021-03-14 09:27:38 -0500

    Change & simplify LatestFrom behavior

--- src/commonMain/kotlin/org/bubenheimer/util/flow/LatestFrom.kt
+++ src/commonMain/kotlin/org/bubenheimer/util/flow/LatestFrom.kt
@@ -1,7 +1,5 @@
 package org.bubenheimer.util.flow
 
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.*
 
@@ -10,11 +8,7 @@ public fun <A, B, R> Flow<A>.waitForLatestFrom(
     transform: suspend (A, B) -> R
 ): Flow<R> = flow {
     coroutineScope {
-        val bStateFlow = b
-            // cancel parent scope on cancellation of child scope
-            .onCompletion { if (it is CancellationException) this@coroutineScope.cancel(it) }
-            .stateIn(this)
-
+        val bStateFlow = b.stateIn(this)
         collect { emit(transform(it, bStateFlow.value)) }
     }
 }
@@ -25,16 +19,8 @@ public fun <A, B, C, R> Flow<A>.waitForLatestFrom(
     transform: suspend (A, B, C) -> R
 ): Flow<R> = flow {
     coroutineScope {
-        val bStateFlow = b
-            // cancel parent scope on cancellation of child scope
-            .onCompletion { if (it is CancellationException) this@coroutineScope.cancel(it) }
-            .stateIn(this)
-
-        val cStateFlow = c
-            // cancel parent scope on cancellation of child scope
-            .onCompletion { if (it is CancellationException) this@coroutineScope.cancel(it) }
-            .stateIn(this)
-
+        val bStateFlow = b.stateIn(this)
+        val cStateFlow = c.stateIn(this)
         collect { emit(transform(it, bStateFlow.value, cStateFlow.value)) }
     }
 }
@@ -44,11 +30,7 @@ public fun <A, B, R> Flow<A>.transformAndWaitForLatestFrom(
     transform: suspend FlowCollector<R>.(A, B) -> Unit
 ): Flow<R> = flow {
     coroutineScope {
-        val bStateFlow = b
-            // cancel parent scope on cancellation of child scope
-            .onCompletion { if (it is CancellationException) this@coroutineScope.cancel(it) }
-            .stateIn(this)
-
+        val bStateFlow = b.stateIn(this)
         collect { transform(it, bStateFlow.value) }
     }
 }
